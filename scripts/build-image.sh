bc7204793e897476261e02769169f68ca04f7503
Ji JinChi <3109952@qq.com>
2026-08-27 23:23:36 +0800

    build: include override-field safety modifier

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -20,6 +20,7 @@ python "$ROOT/scripts/fix_danmaku_recording_path.py" "$UPSTREAM_DIR"
 python "$ROOT/scripts/fix_daily_seq_stream_gears.py" "$UPSTREAM_DIR"
 python "$ROOT/scripts/restore_server_log.py" "$UPSTREAM_DIR"
 python "$ROOT/scripts/apply_product_customizations.py" "$UPSTREAM_DIR"
+python "$ROOT/scripts/fix_override_streamer_fields.py" "$UPSTREAM_DIR"
 python "$ROOT/scripts/fix_recordings_browser.py" "$UPSTREAM_DIR"
 
 docker build \
